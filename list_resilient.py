e7e092fec1d836ca4851ab3be71696fc3484e6ce
Shivanshu26shiv <Shivanshu26shiv@gmail.com>
2020-01-14 03:59:50 +0530

    Update list_resilient.py

--- list_resilient.py
+++ list_resilient.py
@@ -5,13 +5,13 @@ Below code finds out the lowest resilience till 'n' (as given as 100 in the for 
 from datetime import datetime
 import math
 
-tim=datetime.now()
+init_time = datetime.now()
 
 min_resilient = 1
-for input_num in range(2, 100, 2):
-    non_fractionals = [i for i in range(input_num) if math.gcd(input_num, i) == 1]            
+for input_num in range(2, 13, 2):
+    non_fractionals = [i for i in range(input_num) if math.gcd(input_num, i) == 1]
     resilience = len(non_fractionals)/(input_num-1)
-    (exec('min_resilient=resilience'), print('Minimum resilient: ', input_num, 'at', min_resilient)) if min_resilient > resilience else 1
-    
-print('\nTime spent:', datetime.now() - tim)
+    (exec('min_resilient=resilience'), print('Minimum resilient:', input_num, 'at', min_resilient)) if min_resilient > resilience else 1
+
+print('\nTime spent:', datetime.now() - init_time)
 
